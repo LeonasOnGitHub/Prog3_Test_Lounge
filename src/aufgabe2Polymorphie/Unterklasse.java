1ebed97d41706768d1fdb9442f874347558589cd
s0583776 <adobe@gmx.net>
2022-10-14 10:33:19 +0200

    Initial commit

---
+++ src/aufgabe2Polymorphie/Unterklasse.java
@@ -0,0 +1,21 @@
+package aufgabe2Polymorphie;
+
+public class Unterklasse extends Oberklasse {
+	private int x;
+	
+	public Unterklasse()
+	{
+		this.x = 11;
+	}
+	
+	public void methode()
+	{
+		System.out.println(this.x);
+	}
+	
+	public void eineLetzteMethode()
+	{
+		super.eineLetzteMethode();
+		System.out.println(this.x);
+	}
+}
